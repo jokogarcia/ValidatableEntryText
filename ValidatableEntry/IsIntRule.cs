00c7653234ebb4f72fd6ad8f37639aee621958f9
Joaquin Garcia <jokogarcia@gmail.com>
2022-10-24 20:49:39 +0200

    Add isInt Rule Version bump 1.0.9

---
+++ ValidatableEntry/IsIntRule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatableEntry
+{
+    public class IsIntRule : IValidationRule
+    {
+        public string ErrorMessage { get; set; }
+        public int MinValue { get; set; } = int.MinValue;
+        public int MaxValue { get; set; } = int.MaxValue;
+        public bool Validate(string text)
+        {
+            if(int.TryParse(text,out int value))
+            {
+                if(value < MinValue)
+                    return false;
+                if (value > MaxValue)
+                    return false;
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
